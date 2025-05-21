0e960bc63f8a1f8c6474c92ce11d1d7ea416b78a
Zakaria DOUIH <douihzakaria@gmail.com>
2025-05-21 12:29:45 +0100

    correction de style

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="fr">
+    <html lang="fr" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <AuthProvider>
